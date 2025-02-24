16ea7e2871a1064a600fea4a2a8b8215c90ef9c5
jayarani.ys <jayaraniys1@gmail.com>
2025-02-24 19:46:29 +0530

    phase 1 completed

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 let gameSeq=[];
 let userSeq=[];
-
+let btns=["yellow","red","purple","green"];
 let started = false;
 let level=0;
+let h2=document.querySelector("h2");
+
 document.addEventListener("keypress",function(){
     if(started== false){
         console.log("game started");
         started= true;
+
+        levelUp();
     }
 });
+function btnFlash(btn){
+btn.classList.add("flash");
+setTimeout(function(){
+    btn.classList.remove("flash");
+},1000);
+}
+function levelUp(){
+    
+    level++;
+    h2.innerText=`Level ${level}`;
+
+    let randIndx=Math.floor(Math.random()*3);
+    let randColor=btns[randIndx];
+    let randbtn=document.querySelector(`.${randColor}`);
+
+  
+    btnFlash(randbtn);
+
+
+}
